41426a78da102fd31db459357ce96e38327582c1
Nic Costa <nic.costa@gmail.com>
2021-01-15 17:45:27 +0000

    document how to supply username and password for extern http proxy
    added a comment to the CLI help for -extern-http-proxy-uri for
    how to supply a username and password for authenticated proxies.

--- cmd/edge-proxy/main.go
+++ cmd/edge-proxy/main.go
@@ -53,7 +53,7 @@ func main() {
 	flag.StringVar(&tunnelURI, "tunnel-uri", "ws://localhost:8181/connect", "Tunnel address to connect to")
 	flag.StringVar(&proxyURI, "proxy-uri", "", "Root URI to which outgoing HTTP requests should be proxied")
 	flag.StringVar(&proxyAddr, "proxy-listen", "0.0.0.0:8080", "Listen address for HTTP proxy server")
-	flag.StringVar(&externalHTTPProxyURI, "extern-http-proxy-uri", "", "optional external Http proxy for site")
+	flag.StringVar(&externalHTTPProxyURI, "extern-http-proxy-uri", "", "optional external Http proxy for site. For an authenticated proxy, specify the username and password in the URI, as in https://user:pwd@proxy-server:proxy-port")
 	flag.BoolVar(&useL4Proxy, "use-l4-proxy", false, "Use a layer 4 proxy instead of a layer 7 proxy")
 	flag.StringVar(&ca, "ca", "", "Certificate authority for the cloud")
 	flag.StringVar(&certStrategy, "cert-strategy", fog_tls.DefaultDriver(), fmt.Sprintf("Certificate strategy must be one of: %v", fog_tls.Drivers()))
