09494261d9d87e644a549031e9c9a93efa8554f2
Nic Costa <nic.costa@gmail.com>
2021-02-25 22:13:39 +0000

    Allow edge-proxy to start without --proxy-uri

--- cmd/edge-proxy/main.go
+++ cmd/edge-proxy/main.go
@@ -50,55 +50,12 @@ var certStrategyOptions cmd.OptionMap = cmd.OptionMap{}
 var forwardingAddressesMap string
 var httpTunnelAddr string
 
-func main() {
-	flag.StringVar(&tunnelURI, "tunnel-uri", "ws://localhost:8181/connect", "Endpoint to connect to for reverse tunneling")
-	flag.StringVar(&proxyURI, "proxy-uri", "", "Default server to which outgoing HTTP requests should be forwarded.  See forwarding-addresses option for overrides")
-	flag.StringVar(&proxyAddr, "proxy-listen", "0.0.0.0:8080", "Listen address for HTTP proxy server")
-	flag.StringVar(&externalHTTPProxyURI, "extern-http-proxy-uri", "", "optional external Http proxy for site. For an authenticated proxy, specify the username and password in the URI, as in https://user:pwd@proxy-server:proxy-port")
-	flag.BoolVar(&useL4Proxy, "use-l4-proxy", false, "Use a layer 4 proxy instead of a layer 7 proxy")
-	flag.StringVar(&ca, "ca", "", "Certificate authority for the cloud")
-	flag.StringVar(&certStrategy, "cert-strategy", fog_tls.DefaultDriver(), fmt.Sprintf("Certificate strategy must be one of: %v", fog_tls.Drivers()))
-	flag.Var(&certStrategyOptions, "cert-strategy-options", "Can be specified one or more times. Must be a key-value pair (<key>=<value>)")
-	flag.StringVar(&forwardingAddressesMap, "forwarding-addresses", "{}", "Map of local address to forwarded address for outgoing HTTP requests. For each forwarding request received at proxy-listen, the destination URI in the request is rewritten based on this map, where the destination server is replaced with the value of the corresponding key.  If the destination server isn't found in this map, then the value of proxy-uri is used.  Must be a json string")
-	flag.StringVar(&httpTunnelAddr, "http-tunnel-listen", "localhost:8888", "Listen address for HTTP (CONNECT) tunnel server")
-	flag.Parse()
-
-	if proxyURI == "" {
-		fmt.Printf("proxy-uri must be provided\n")
-
-		os.Exit(1)
-	}
-
-	proxyURIParsed, err := url.Parse(proxyURI)
-
-	if err != nil {
-		fmt.Printf("proxy-uri invalid: %s\n", err.Error())
-
-		os.Exit(1)
-	}
-
-	if tunnelURI == "" {
-		fmt.Printf("tunnel-uri must be provided\n")
-
-		os.Exit(1)
-	}
-
-	if externalHTTPProxyURI != "" {
-		_, err := url.Parse(externalHTTPProxyURI)
-		if err != nil {
-			fmt.Printf("extern-http-proxy-uri invalid: %s\n", err.Error())
-
-			os.Exit(1)
-		}
-	}
-
+func initEdgeProxy(proxyURIParsed *url.URL) {
 	var forwardingAddressesMapParsed map[string]string
 
-	err = json.Unmarshal([]byte(forwardingAddressesMap), &forwardingAddressesMapParsed)
-
+	err := json.Unmarshal([]byte(forwardingAddressesMap), &forwardingAddressesMapParsed)
 	if err != nil {
 		fmt.Printf("forwarding-addresses invalid: %s\n", err.Error())
-
 		os.Exit(1)
 	}
 
@@ -106,30 +63,22 @@ func main() {
 
 	if ca != "" {
 		fmt.Printf("Loading CA from %s\n", ca)
-
 		caList, err = loadCA(ca)
-
 		if err != nil {
 			fmt.Printf("Unable to load CA from %s: %s\n", ca, err.Error())
-
 			os.Exit(1)
 		}
 	}
 
-	ch := make(chan bool)
-
 	certificate, renewals, err := fog_tls.MakeCertificate(certStrategy, fog_tls.CertStrategyConfig(certStrategyOptions))
-
 	if err != nil {
 		fmt.Printf("Unable to initialize client certificate: %s\n", err.Error())
-
 		os.Exit(1)
 	}
 
 	go func() {
 		for {
 			fmt.Printf("Establishing edge-proxy reverse tunnel (tunnelURI=%s)\n", tunnelURI)
-
 			remotedialer.ClientConnect(tunnelURI, http.Header{}, &websocket.Dialer{
 				NetDial: func(network, address string) (net.Conn, error) {
 					netDialer := &net.Dialer{}
@@ -137,12 +86,10 @@ func main() {
 				},
 			}, func(string, string) bool { return true }, func(ctx context.Context) error {
 				fmt.Printf("edge-proxy reverse tunnel established\n")
-
 				return nil
 			})
 
 			fmt.Printf("edge-proxy tunnel exited. Attempting to reestablish tunnel in %d seconds...\n", TunnelBackoffSeconds)
-
 			time.Sleep(time.Second * TunnelBackoffSeconds)
 		}
 	}()
@@ -155,19 +102,15 @@ func main() {
 				c := <-renewals
 				cert = c
 				fmt.Print("edge-proxy received a renewal cert. Proxy server should be re-launched with the new cert...\n")
-
 				cancelChildCtx()
 			}()
 
 			if useL4Proxy {
 				fmt.Printf("Starting edge TLS proxy (proxyAddr=%s, proxyURI=%s)\n", proxyAddr, proxyURI)
-
 				server.RunEdgeTLSProxyServer(childCtx, proxyAddr, proxyURIParsed, caList, cert)
-
 				fmt.Printf("Edge TLS proxy server exited\n")
 			} else {
 				fmt.Printf("Starting edge HTTP proxy (proxyAddr=%s, proxyURI=%s)\n", proxyAddr, proxyURI)
-
 				proxyForEdge := func(req *http.Request) (*url.URL, error) {
 
 					if externalHTTPProxyURI != "" {
@@ -191,10 +134,62 @@ func main() {
 			<-time.After(time.Second * ServerBackoffSeconds)
 		}
 	}(certificate)
+}
 
+func initHttpProxy(httpTunnelAddr string) {
 	server.StartHTTPTunnel(httpTunnelAddr)
-	<-ch
+}
 
+func main() {
+	flag.StringVar(&tunnelURI, "tunnel-uri", "ws://localhost:8181/connect", "Endpoint to connect to for reverse tunneling")
+	flag.StringVar(&proxyURI, "proxy-uri", "", "Default server to which outgoing HTTP requests should be forwarded.  See forwarding-addresses option for overrides")
+	flag.StringVar(&proxyAddr, "proxy-listen", "0.0.0.0:8080", "Listen address for HTTP proxy server")
+	flag.StringVar(&externalHTTPProxyURI, "extern-http-proxy-uri", "", "optional external Http proxy for site. For an authenticated proxy, specify the username and password in the URI, as in https://user:pwd@proxy-server:proxy-port")
+	flag.BoolVar(&useL4Proxy, "use-l4-proxy", false, "Use a layer 4 proxy instead of a layer 7 proxy")
+	flag.StringVar(&ca, "ca", "", "Certificate authority for the cloud")
+	flag.StringVar(&certStrategy, "cert-strategy", fog_tls.DefaultDriver(), fmt.Sprintf("Certificate strategy must be one of: %v", fog_tls.Drivers()))
+	flag.Var(&certStrategyOptions, "cert-strategy-options", "Can be specified one or more times. Must be a key-value pair (<key>=<value>)")
+	flag.StringVar(&forwardingAddressesMap, "forwarding-addresses", "{}", "Map of local address to forwarded address for outgoing HTTP requests. For each forwarding request received at proxy-listen, the destination URI in the request is rewritten based on this map, where the destination server is replaced with the value of the corresponding key.  If the destination server isn't found in this map, then the value of proxy-uri is used.  Must be a json string")
+	flag.StringVar(&httpTunnelAddr, "http-tunnel-listen", "localhost:8888", "Listen address for HTTP (CONNECT) tunnel server")
+	flag.Parse()
+
+	var proxyURIParsed *url.URL
+	if proxyURI == "" {
+		fmt.Printf("proxy-uri was not provided, starting in CONNECT only mode\n")
+	} else {
+		p, err := url.Parse(proxyURI)
+		if err != nil {
+			fmt.Printf("proxy-uri invalid: %s\n", err.Error())
+			os.Exit(1)
+		}
+		proxyURIParsed = p
+	}
+
+	if tunnelURI == "" {
+		fmt.Printf("tunnel-uri must be provided\n")
+
+		os.Exit(1)
+	}
+
+	if externalHTTPProxyURI != "" {
+		_, err := url.Parse(externalHTTPProxyURI)
+		if err != nil {
+			fmt.Printf("extern-http-proxy-uri invalid: %s\n", err.Error())
+
+			os.Exit(1)
+		}
+	}
+
+	if proxyURIParsed != nil {
+		initEdgeProxy(proxyURIParsed)
+	}
+
+	if httpTunnelAddr != "" {
+		initHttpProxy(httpTunnelAddr)
+	}
+
+	blockforever := make(chan struct{})
+	<-blockforever
 }
 
 func loadCA(caFile string) (*x509.CertPool, error) {
