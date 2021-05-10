120ffe3014a49a95e2b28fdb1744d2be3ff8e4c2
J. Michael Welsh <mike.welsh@arm.com>
2021-05-10 20:53:27 +0000

    wstunnel proxy cli
    Adding command line paramaters for the new wstunnel proxy in
    edge-proxy

--- cmd/edge-proxy/main.go
+++ cmd/edge-proxy/main.go
@@ -19,6 +19,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -50,18 +51,22 @@ var certStrategyOptions cmd.OptionMap = cmd.OptionMap{}
 var forwardingAddressesMap string
 var httpTunnelAddr string
 var proxyOnlyMode bool
+var wstunnelConnect string
+var wstunnelListen string
 
 func main() {
-	flag.StringVar(&tunnelURI, "tunnel-uri", "ws://localhost:8181/connect", "Endpoint to connect to for reverse tunneling")
-	flag.StringVar(&proxyURI, "proxy-uri", "", "Default server to which outgoing HTTP requests should be forwarded.  See forwarding-addresses option for overrides")
-	flag.StringVar(&proxyAddr, "proxy-listen", "0.0.0.0:8080", "Listen address for HTTP proxy server")
-	flag.StringVar(&externalHTTPProxyURI, "extern-http-proxy-uri", "", "optional external Http proxy for site. For an authenticated proxy, specify the username and password in the URI, as in https://user:pwd@proxy-server:proxy-port")
+	flag.StringVar(&tunnelURI, "reversetunnel-connect", "ws://localhost:8181/connect", "Endpoint to connect to for reverse tunneling")
+	flag.StringVar(&proxyURI, "httpproxy-connect", "", "Default server to which outgoing HTTP requests should be forwarded.  See forwarding-addresses option for overrides")
+	flag.StringVar(&proxyAddr, "httpproxy-listen", "0.0.0.0:8080", "Listen address for HTTP proxy server")
+	flag.StringVar(&externalHTTPProxyURI, "extern-httpproxy-connect", "", "optional external Http proxy for site. For an authenticated proxy, specify the username and password in the URI, as in https://user:pwd@proxy-server:proxy-port")
 	flag.BoolVar(&useL4Proxy, "use-l4-proxy", false, "Use a layer 4 proxy instead of a layer 7 proxy")
 	flag.StringVar(&ca, "ca", "", "Certificate authority for the cloud")
 	flag.StringVar(&certStrategy, "cert-strategy", fog_tls.DefaultDriver(), fmt.Sprintf("Certificate strategy must be one of: %v", fog_tls.Drivers()))
 	flag.Var(&certStrategyOptions, "cert-strategy-options", "Can be specified one or more times. Must be a key-value pair (<key>=<value>)")
 	flag.StringVar(&forwardingAddressesMap, "forwarding-addresses", "{}", "Map of local address to forwarded address for outgoing HTTP requests. For each forwarding request received at proxy-listen, the destination URI in the request is rewritten based on this map, where the destination server is replaced with the value of the corresponding key.  If the destination server isn't found in this map, then the value of proxy-uri is used.  Must be a json string")
-	flag.StringVar(&httpTunnelAddr, "http-tunnel-listen", "localhost:8888", "Listen address for HTTP (CONNECT) tunnel server")
+	flag.StringVar(&httpTunnelAddr, "httptunnel-listen", "localhost:8888", "Listen address for HTTP (CONNECT) tunnel server")
+	flag.StringVar(&wstunnelConnect, "wstunnel-connect", "", "Default server to which outgoing wstunnel proxy requests should be forwarded.")
+	flag.StringVar(&wstunnelListen, "wstunnel-listen", "0.0.0.0:9999", "Listen address for wstunnel proxy server")
 	flag.Parse()
 
 	proxyOnlyMode = false
@@ -94,6 +99,17 @@ func main() {
 
 	ch := make(chan bool)
 	<-ch
+}
+
+func startWSTunnel(connect string, listen string) error {
+	var err error
+
+	if connect == "" || listen == "" {
+		err = errors.New("empty parameters")
+		return err
+	}
+
+	return nil
 }
 
 func startEdgeProxyReverseTunnel(ca string, proxyURI string, forwardingAddressesMap string, certStrategy string, certStrategyOptions cmd.OptionMap) error {
